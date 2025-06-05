2818bf4df131f30883eaa1774f180d49efe1d419
mariamrashid <giannarashid@gmail.com>
2025-06-05 12:28:56 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';// Declare currentUser
+
+// Build welcome message using interpolation and add "!"
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Make an all-uppercase version of welcomeMessage
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Short greeting that uses only the first initial of currentUser
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
